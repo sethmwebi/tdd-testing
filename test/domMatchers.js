4f1ce79d980d160930a7db9cdf600454a9eaf0af
Seth Mwebi <sethmwebi27@gmail.com>
2023-03-27 21:18:18 +0300

    testing useeffect

--- test/domMatchers.js
+++ test/domMatchers.js
@@ -1,5 +1,12 @@
-import { toContainText } from "./matchers/toContainText"
-import { toHaveClass } from "./matchers/toHaveClass"
-import { toBeElementWithTag } from "./matchers/toBeElementWithTag"
+import { toContainText } from "./matchers/toContainText";
+import { toHaveClass } from "./matchers/toHaveClass";
+import { toBeElementWithTag } from "./matchers/toBeElementWithTag";
+import { toBeRenderedWithProps, toBeFirstRenderedWithProps } from "./matchers/toBeRenderedWithProps";
 
-expect.extend({ toContainText, toHaveClass, toBeElementWithTag })
+expect.extend({
+	toContainText,
+	toHaveClass,
+	toBeElementWithTag,
+	toBeRenderedWithProps,
+	toBeFirstRenderedWithProps
+});
--- test/reactTestExtensions.js
+++ test/reactTestExtensions.js
@@ -2,32 +2,43 @@ import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
 
 export let container;
+let reactRoot;
 
 export const initializeReactContainer = () => {
 	container = document.createElement("div");
 	document.body.replaceChildren(container);
+
+	reactRoot = ReactDOM.createRoot(container);
 };
 
 export const render = (component) =>
-	act(() => ReactDOM.createRoot(container).render(component));
+	act(() => reactRoot.render(component));
 
-export const click = (element) => act(() => element.click());
+export const renderAndWait = (component) =>
+	act(async () => reactRoot.render(component));
 
-export const clickAndWait = async (element) => act(async () => click(element))
+export const click = (element) =>
+	act(() => element.click());
 
-export const element = (selector) => document.querySelector(selector);
+export const clickAndWait = async (element) =>
+	act(async () => click(element));
+
+export const element = (selector) =>
+	document.querySelector(selector);
 
 export const elements = (selector) =>
 	Array.from(document.querySelectorAll(selector));
 
-export const typesOf = (elements) => elements.map((element) => element.type);
+export const typesOf = (elements) =>
+	elements.map((element) => element.type);
 
 export const textOf = (elements) =>
 	elements.map((element) => element.textContent);
 
 export const form = (id) => element("form");
 
-export const field = (fieldName) => form().elements[fieldName];
+export const field = (fieldName) =>
+	form().elements[fieldName];
 
 export const submit = (formElement) => {
 	const event = new Event("submit", {
@@ -39,17 +50,26 @@ export const submit = (formElement) => {
 	return event;
 };
 
-export const submitAndWait = async (formElement) => act(async() => submit(formElement))
+export const submitAndWait = async (formElement) =>
+	act(async () => submit(formElement));
 
-export const submitButton = () => element("input[type=submit]");
+export const submitButton = () =>
+	element("input[type=submit]");
 
 const originalValueProperty = (reactElement) => {
-	const prototype = Object.getPrototypeOf(reactElement);
-	return Object.getOwnPropertyDescriptor(prototype, "value");
+	const prototype =
+		Object.getPrototypeOf(reactElement);
+	return Object.getOwnPropertyDescriptor(
+		prototype,
+		"value"
+	);
 };
 
 export const change = (target, value) => {
-	originalValueProperty(target).set.call(target, value);
+	originalValueProperty(target).set.call(
+		target,
+		value
+	);
 
 	const event = new Event("change", {
 		target,
@@ -59,4 +79,5 @@ export const change = (target, value) => {
 	act(() => target.dispatchEvent(event));
 };
 
-export const labelFor = (formElement) => element(`label[for=${formElement}]`);
+export const labelFor = (formElement) =>
+	element(`label[for=${formElement}]`);
